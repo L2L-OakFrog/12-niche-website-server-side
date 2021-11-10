280613e2c85f6b3f13ee4c48d1e1aa177bc89e73
L2L_OakFrog <rafiqahamed.tanim@gmail.com>
2021-11-10 12:55:46 +0600

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+const cors = require('cors');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+app.get('/', (req, res) => {
+    res.send("Welcome To Jerin's parlour")
+})
+
+app.listen(port, () => {
+    console.log(`listening http://localhost:${port}`)
+})
